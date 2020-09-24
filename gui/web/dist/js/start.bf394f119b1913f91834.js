8e71ac202d709dd1f4548d7b66274019b12b3a51
Messi <neomessi@outlook.com>
2020-09-24 15:37:31 -0400

    added dist files

--- gui/web/dist/js/start.bf394f119b1913f91834.js
+++ gui/web/dist/js/start.bf394f119b1913f91834.js
@@ -90,13 +90,12 @@
 
 console.log('starting up... 2:43pm');
 
-const test = (msg) => {
-    console.log(msg);
-}
-
-// const arr = [2, 4, 6 ,8];
+const test = msg => {
+  console.log(msg);
+}; // const arr = [2, 4, 6 ,8];
 //const arr2 = [...arr];
 //console.log(arr2);
+
 
 test("abc12345");
 

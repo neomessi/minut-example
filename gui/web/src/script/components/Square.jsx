cc992eb9988be29d1c101db1de7cd2d5cef26aed
Messi <neomessi@outlook.com>
2021-01-22 12:52:23 -0500

    bootstrap styles and modal

--- gui/web/src/script/components/Square.jsx
+++ gui/web/src/script/components/Square.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import { Modal } from 'bootstrap';
 
 export default (props) => {
-    console.log(props)
 
     return (
-    <span>
-        { props.number && typeof parseInt(props.number) == 'number' ? Math.pow(props.number,2) : 'Invalid number' } { !!props.unit && props.unit }
-    </span>
+        <div>
+            <span>
+                { props.number && typeof parseInt(props.number) == 'number' ? Math.pow(props.number,2) : 'Invalid number' }
+            </span>
+
+            {/* modal */}
+            { props.unit && (
+                <div>
+                    <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        click to see
+                    </button>
+
+                    <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Units</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            {props.unit}
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            )}
+        </div>
     )
 }
